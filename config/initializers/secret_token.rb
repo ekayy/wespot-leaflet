345332ee6648ce671cb82eab1e3692b6322032db
admin <eric@ubuntu.(none)>
2012-10-27 16:11:23 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wespot::Application.config.secret_token = '787f28b7f23028d65ee19df44912ef195c9a354c96eb15438a74af113aedea774e6ae822410bdb279cf5894f025c5f924695cb9cd25527fdcecfd8467be7bae9'
