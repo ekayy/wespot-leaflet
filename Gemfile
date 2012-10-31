bb6c31381febcbc597a859c4e0d9aa1b5d9eccf8
admin <eric@ubuntu.(none)>
2012-10-31 04:15:52 -0700

    Added tags

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem "cancan", ">= 1.6.8"
 gem "rolify", ">= 3.2.0"
 gem "simple_form", ">= 2.0.3"
 gem 'pg'
+gem 'acts-as-taggable-on', '~> 2.3.1'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,8 @@
 class Place < ActiveRecord::Base
-  attr_accessible :business_name, :street, :city, :zip, :state, :country, :phone
+  attr_accessible :business_name, :street, :city, :zip, :state, :country, :phone, :tag_list
 
   belongs_to :user
   has_many :comments, as: :commentable
+
+  acts_as_taggable
 end
