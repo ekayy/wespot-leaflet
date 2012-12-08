fa890cd10e60c8fb29a131938a210898c821d747
admin <eric@ubuntu.(none)>
2012-12-07 22:48:45 -0800

    relationships1

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,13 +3,13 @@
 	FactoryGirl.define do
 	  factory :user do
 	    name 'Test User'
-	    email 'usertest@example.com'
+	    email 'user@example.com'
 	    password 'secret'
 	    password_confirmation 'secret'
 	    # required if the Devise Confirmable module is used
 	    # confirmed_at Time.now
 		 # factory :admin do
 	   #      after(:create) {|user| user.add_role(:admin)}
-	    end
+	    # end
 	  end
 	end
